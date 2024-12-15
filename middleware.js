061862bfb4bb414704826124f077ad1a855c2105
Yalamanchili Surya Teja <suryatejaprogramming@gmail.com>
2024-12-15 12:00:00 +0530

    Prod commit middleware

---
+++ middleware.js
@@ -0,0 +1,20 @@
+const admin = require("firebase-admin");
+
+const authenticateToken = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+
+  const idToken = authHeader.split("Bearer ")[1];
+
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(idToken);
+    req.user = decodedToken;
+    next();
+  } catch (error) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+};
+
+module.exports = authenticateToken;
