015c9c991e0c4ae7a30cc487b0674f89c9f6ae86
Sarthak S Kumar <sarthakskumar7@gmail.com>
2025-10-08 07:15:10 +0530

    feat: add application constants

---
+++ constants/index.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+
+const PORT = process.env.PORT || 4200;
+
+const COLLECTION_NAMES = {
+  USERS: "users",
+  CATEGORY: "category",
+  DISHES: "dishes",
+  ORDERS: "orders",
+  ORDER: "order",
+  CART: "cart",
+  LOCATION: "location",
+  PROMO_CODES: "promoCodes",
+  GOLD_PRICE: "goldprice",
+  MINI_GAMES: "miniGames",
+  REWARDS: "rewards",
+  NOTIFICATIONS: "notifications",
+  USER_TOKENS: "userTokens",
+};
+
+const MAX_MINI_GAMES = 6;
+
+module.exports = {
+  PORT,
+  COLLECTION_NAMES,
+  MAX_MINI_GAMES,
+};
